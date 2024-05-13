ec9b78cb27e11847738fad2dcd8aa199378aa952
Nattapon Jaroenchai <b_nj18@live.com>
2024-05-13 18:22:44 -0500

    Update Dataset_val.py

--- libs/Dataset_val.py
+++ libs/Dataset_val.py
@@ -8,7 +8,8 @@ import torchvision.transforms.functional as TF
 import torchvision.transforms as transforms
 from h5image import H5Image
 from random import choice, sample
-from Normalization import MinMaxNormalizer, BasicNormalizer, ImageNetNormalizer
+from skimage.transform import resize
+from Normalization import BasicNormalizer, ImageNetNormalizer, MinMaxNormalizer
 
 class PatchDataGenerator:
     def __init__(self, data_dir='/projects/bbym/shared/data/commonPatchData/', patch_size=256, overlap=15, norm_type='imagenet', valid_patch_rate=0.75,
@@ -26,13 +27,13 @@ class PatchDataGenerator:
         self.horizontal_flip_rate = horizontal_flip_rate
         self.rotation_rate = rotation_rate
         self.hue_factor = hue_factor
-        self.legend_trasform =transforms.Compose([transforms.ToPILImage(), transforms.Resize((self.patch_size, self.patch_size)), transforms.ToTensor()])
+        self.legend_trasform =transforms.Compose([transforms.ToPILImage(), transforms.Resize((self.patch_size, self.patch_size))])
 
         self.mapsh5i, self.map_names = self._load_maps(self.data_dir)
         self.patches = self._generate_patches(self.map_names, self.mapsh5i)
 
         print("*****Training Initialization ****")
-        print("data path :", self.data_dir)
+        print(" data dir :", self.data_dir)
         print("*********************************")
 
     def _load_maps(self, h5_dir):
@@ -64,7 +65,7 @@ class PatchDataGenerator:
 
         for file in files_name:
             mapname = os.path.basename(file).replace(".hdf5", "")
-            mapsh5i[mapname] = H5Image(file, "r") 
+            mapsh5i[mapname] = H5Image(file, "r")
 
         print("Setup complete. Number of maps loaded:", len(mapsh5i))
         return mapsh5i, list(mapsh5i.keys())
@@ -76,30 +77,26 @@ class PatchDataGenerator:
                 patches = h5image_obj[map_name].get_patches(map_name)
                 cor = h5image_obj[map_name].get_map_corners(map_name)
 
-                # Filter patches based on label presence:
-                valid_patches = []
-                for layer in patches:
-                    if layer.endswith('_poly'):
-                        for row, column in patches[layer]:
-                                # Retrieve the label patch for the given row, column, map name, and layer
-                            label_patch = h5image_obj[map_name].get_patch(
-                                row, column, map_name, layer)
-                            # Count the pixels of interest (value 1) in the label patch
-                            # Check if the label patch has exactly 100 pixels of interest
-                            if np.sum(label_patch) >= 100:
-                                valid_patches.append((map_name, layer, row, column))
+            # Filter patches based on label presence:
+            valid_patches = []
+            for layer in patches:
+                if layer.endswith('_poly'):
+                    for row, column in patches[layer]:
+                        # Retrieve the label patch for the given row, column, map name, and layer
+                        label_patch = h5image_obj[map_name].get_patch(row, column, map_name, layer)
+                        # Count the pixels of interest (value 1) in the label patch
+                        if np.sum(label_patch) >= 100:  # Check if the label patch has exactly 100 pixels of interest
+                            valid_patches.append((map_name, layer, row, column))
 
-                # Sample patches with a mix of valid and random locations:
-                valid_layers = [
-                    key for key in patches.keys() if key.endswith('_poly')]
-                random_patches = [(map_name, choice(valid_layers),
+            # Sample patches with a mix of valid and random locations:
+            valid_layers = [key for key in patches.keys() if key.endswith('_poly')]
+            random_patches = [(map_name, choice(valid_layers),
                                 random.randint(cor[0][0], cor[1][0]),
                                 random.randint(cor[0][1], cor[1][1]))
                                 for _ in range(len(valid_patches))]
 
-                n = len(valid_patches)
-                map_patches += sample(valid_patches, int(n * self.valid_patch_rate)) + \
-                    sample(random_patches, int(n * (1 - self.valid_patch_rate)))
+            n = len(valid_patches)
+            map_patches += sample(valid_patches, int(n * self.valid_patch_rate)) +  sample(random_patches, int(n * (1 - self.valid_patch_rate)))
 
         return map_patches
 
@@ -127,9 +124,10 @@ class PatchDataGenerator:
 
         # Color Jitter
         if random.random() < self.hue_factor:
-            map_patch = TF.adjust_hue(map_patch, hue_factor=self.hue_factor)
-            legend_patch = TF.adjust_hue(
-                legend_patch, hue_factor=self.hue_factor)
+            random_brightness = random.uniform(0.8, 1.2)
+            map_patch = TF.adjust_brightness(map_patch, brightness_factor=random_brightness)
+            # map_patch = TF.adjust_hue(map_patch, hue_factor=self.hue_factor)
+            # legend_patch = TF.adjust_hue(legend_patch, hue_factor=self.hue_factor)
 
         return map_patch, legend_patch, label_patch
 
@@ -140,48 +138,46 @@ class PatchDataGenerator:
         print(self.patches[index])
         map_name, layer_name, row, column = self.patches[index]
         # Get map patch (using h5py syntax)
-        map_patch = torch.from_numpy(
-            self.mapsh5i[map_name].get_patch(row, column, map_name))
+        map_patch = torch.from_numpy(self.mapsh5i[map_name].get_patch(row, column, map_name))
         map_patch = map_patch.permute(2, 0, 1)
-        print("map_patch", map_patch.shape)
+        # print("map_patch", map_patch.shape)
 
         # Get legend patch
-        legend_patch = torch.from_numpy(
-            self.mapsh5i[map_name].get_legend(map_name, layer_name))
-        legend_patch = legend_patch.permute(2, 0, 1)
-        legend_patch_resize = self.legend_trasform(legend_patch)
-        print("legend_patch_resize", legend_patch_resize.shape)
+        legend_patch = self.mapsh5i[map_name].get_legend(map_name, layer_name)
+        # print("legend_patch:", legend_patch.shape)
+        legend_patch = resize(legend_patch, (self.patch_size, self.patch_size), preserve_range= True, anti_aliasing=True)
+        # print("legend_patch resized:", legend_patch.shape)
+        legend_patch = torch.from_numpy(legend_patch)
+        legend_patch_resize = legend_patch.permute(2, 0, 1)
+        # legend_patch_resize = self.legend_trasform(legend_patch)
+        # print("legend_patch_resize", legend_patch_resize.shape)
 
         # Get label
-        label_patch = torch.from_numpy(
-            self.mapsh5i[map_name].get_patch(row, column, map_name, layer_name))
+        label_patch = torch.from_numpy(self.mapsh5i[map_name].get_patch(row, column, map_name, layer_name))
         # Add new axis to label patch
         label_patch = label_patch.unsqueeze(0)
-        print("label_patch", label_patch.shape)
+        # print("label_patch", label_patch.shape)
 
         # Apply augmentation
         if self.augment:
-            map_patch, legend_patch, label_patch = self._augment_data(
-                map_patch, legend_patch_resize, label_patch)
+            # map_patch, legend_patch, label_patch = map_patch, legend_patch_resize, label_patch
+            map_patch, legend_patch, label_patch = self._augment_data(map_patch, legend_patch_resize, label_patch)
 
         # Normalize the map patch and legend
         if self.norm_type == 'imagenet':
             normalizer = ImageNetNormalizer()
             map_patch_norm = normalizer.normalize(map_patch)
             legend_patch_norm = normalizer.normalize(legend_patch)
-            data_patch = np.concatenate(
-                [map_patch_norm, legend_patch_norm], axis=0)
+            data_patch = np.concatenate([map_patch_norm, legend_patch_norm], axis=0)
 
         elif self.norm_type == 'basic':
             normalizer = MinMaxNormalizer(0, 255, -1, 1)
             data_patch = np.concatenate([map_patch, legend_patch], axis=0)
-            data_patch = normalizer.normalize(
-                data_patch)  # Potential problem here
+            data_patch = normalizer.normalize(data_patch)
 
         else:
             normalizer = BasicNormalizer()
             data_patch = np.concatenate([map_patch, legend_patch], axis=0)
-            data_patch = normalizer.normalize(
-                data_patch)  # Potential problem here
+            data_patch = normalizer.normalize(data_patch)
 
         return data_patch, label_patch
