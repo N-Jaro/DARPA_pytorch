186e7b4c4c4194606544ba5cf748c2ac5f07e008
Nattapon Jaroenchai <b_nj18@live.com>
2024-05-15 17:27:34 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=u_transformer_training    # Job name
+#SBATCH --job-name=u_transformer_training   
 #SBATCH --time=20:00:00
 #SBATCH --partition=a100
 #SBATCH --account=bbym-hydro
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
 #SBATCH --gpus-per-node=2
-#SBATCH --output=u_transformer_%j.out         # Output file name (%j expands to jobID)
-#SBATCH --error=u_transformer_%j.err          # Error file name (%j expands to jobID)
+#SBATCH --output=u_transformer_%j.out   
 
 # Load required modules
 module load anaconda3_gpu cudnn
