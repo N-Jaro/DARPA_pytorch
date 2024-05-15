76e492c9d7d063f6b46f5524dccaf67051131413
Nattapon Jaroenchai <b_nj18@live.com>
2024-05-15 17:10:17 -0500

    Create train.sh
    Training for sbatch

---
+++ train.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --job-name=u_transformer_training    # Job name
+#SBATCH --time=20:00:00
+#SBATCH --partition=a100
+#SBATCH --account=bbym-hydro
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus-per-node=2
+#SBATCH --output=/projects/bbym/nathanj/attentionUnet/u_transformer_%j.out         # Output file name (%j expands to jobID)
+#SBATCH --error=u_transformer_%j.err          # Error file name (%j expands to jobID)
+
+# Load required modules
+module load anaconda3_gpu cudnn
+pip install lightning
+
+# Variables for the script
+TRAIN_DATA_DIR="/projects/bbym/shared/data/commonPatchData/"
+VAL_DATA_DIR="/projects/bbym/shared/data/commonPatchData/validation"
+CHECKPOINT_DIR="checkpoints"
+PROJECT_NAME="U_Transformer_Segmentation"
+NAME_ID="Utransformer_256_32_w_val"
+BATCH_SIZE=32
+LEARNING_RATE=1e-3
+NUM_EPOCHS=150
+PATCH_SIZE=256
+OVERLAP=32
+NORM_TYPE="basic"
+HUE_FACTOR=0.2
+VALID_PATCH_RATE=1
+AUGMENT=true
+NUM_WORKERS=12
+DYNAMIC_VALID_PATCH_RATE=true
+
+# Run the training script
+python lit_training.py \
+    --train_data_dir $TRAIN_DATA_DIR \
+    --val_data_dir $VAL_DATA_DIR \
+    --checkpoint_dir $CHECKPOINT_DIR \
+    --project_name $PROJECT_NAME \
+    --name_id $NAME_ID \
+    --batch_size $BATCH_SIZE \
+    --learning_rate $LEARNING_RATE \
+    --num_epochs $NUM_EPOCHS \
+    --patch_size $PATCH_SIZE \
+    --overlap $OVERLAP \
+    --norm_type $NORM_TYPE \
+    --hue_factor $HUE_FACTOR \
+    --valid_patch_rate $VALID_PATCH_RATE \
+    --augment $AUGMENT \
+    --num_workers $NUM_WORKERS \
+    --dynamic_valid_patch_rate $DYNAMIC_VALID_PATCH_RATE
