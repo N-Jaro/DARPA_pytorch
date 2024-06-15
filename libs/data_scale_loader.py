c786c7ed8aa71e368e64490f3f0bd1c8b42fc115
Nattapon J <nj7@illinois.edu>
2024-06-15 14:07:04 -0500

    Training with different scales.

---
+++ libs/data_scale_loader.py
@@ -0,0 +1,213 @@
+import os
+import glob
+import random
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from h5image import H5Image
+from random import choice, sample
+from skimage.transform import resize
+from Normalization import MinMaxNormalizer, BasicNormalizer, ImageNetNormalizer
+import torchvision.transforms.functional as TF
+
+class ScaleDataGenerator(Dataset):
+    def __init__(self, map_names, mapsh5i, batch_size=32, patch_size=256, overlap=15, norm_type='imagenet', valid_patch_rate=0.75,
+                 augment=True, vertical_flip_rate=0.4, horizontal_flip_rate=0.4, rotation_rate=0.4, hue_factor=0.2, test_flag=False):
+
+        self.patch_size = patch_size
+        self.overlap = overlap
+        self.batch_size = batch_size
+        self.valid_patch_rate = valid_patch_rate
+        self.augment = augment
+        self.norm_type = norm_type
+        self.vertical_flip_rate = vertical_flip_rate
+        self.horizontal_flip_rate = horizontal_flip_rate
+        self.rotation_rate = rotation_rate
+        self.hue_factor = hue_factor
+        self.test_flag = test_flag
+
+        self.patch_sizes = [128, 256, 512, 1024]  # Supported patch sizes
+
+        self.mapsh5i = mapsh5i
+        self.map_names = map_names
+
+        self.patches = self._generate_patches(self.map_names, self.mapsh5i)
+
+    def _load_maps(self, h5_dir):
+        mapsh5i = {}
+        exclude_files = [
+            "24_Black Crystal_2014_p43.hdf5", "26_Black Crystal_2018_6.hdf5",
+            "46_Coosa_2015_11 74.hdf5", "DMEA2328_OldLeydenMine_CO.hdf5",
+            "AZ_PrescottNF_basemap.hdf5", "CarlinVanadiumNI43-101Jun2020.hdf5",
+            "Genesis_fig7_1.hdf5", "Genesis_fig9_1.hdf5", "Genesis_fig9_2.hdf5",
+            "Genesis_fig9_3.hdf5", "Genesis_fig9_4.hdf5", "Genesis_fig9_5.hdf5",
+            "Titac_2011_fig7_6.hdf5", "Trend_2005_fig4_3.hdf5", "Trend_2005_fig6_4.hdf5",
+            "Trend_2005_fig7_2.hdf5", "Trend_2007_fig10_2.hdf5", "USGS_GF-7_2.hdf5",
+            "CA_SanJone"
+        ]
+
+        for size in self.patch_sizes:
+            sub_dir = os.path.join(h5_dir, str(size), str(self.overlap))
+            size_mapsh5i = {}
+            files_name = [f for f in glob.glob(os.path.join(sub_dir, "*.hdf5")) if os.path.basename(f) not in exclude_files]
+            for file in files_name:
+                path = os.path.join(sub_dir, file)
+                mapname = os.path.basename(path).replace(".hdf5", "")
+                size_mapsh5i[mapname] = H5Image(path, "r")
+            mapsh5i[size] = size_mapsh5i  
+
+        print("Setup complete. Number of maps loaded:", len(mapsh5i))
+        map_names = list(mapsh5i[self.patch_size].keys())  # Default to input patch size for map_names
+        return mapsh5i, map_names
+
+    def _generate_patches_for_map(self, map_name, mapsh5i):
+        map_patches = []
+        for size in self.patch_sizes:
+            if map_name in mapsh5i[size]:
+                patches = mapsh5i[size][map_name].get_patches(map_name)
+                cor = mapsh5i[size][map_name].get_map_corners(map_name)
+
+                valid_patches = []
+                for layer in patches:
+                    if layer.endswith('_poly'):
+                        for row, column in patches[layer]:
+                            label_patch = mapsh5i[size][map_name].get_patch(row, column, map_name, layer)
+                            if np.sum(label_patch) >= 100: 
+                                valid_patches.append((map_name, layer, row, column, size)) 
+
+                valid_layers = [key for key in patches.keys() if key.endswith('_poly')]
+                random_patches = [(map_name, choice(valid_layers), 
+                                    sample(range(cor[0][0], cor[1][0]), 1)[0],  
+                                    sample(range(cor[0][1], cor[1][1]), 1)[0],
+                                    size)  
+                                    for _ in range(len(valid_patches))]
+                
+                n = len(valid_patches)
+                map_patches += sample(valid_patches, int(n * self.valid_patch_rate)) + sample(random_patches, int(n * (1 - self.valid_patch_rate)))
+
+        return map_patches
+
+    def _generate_patches(self, map_names, mapsh5i):
+        all_patches = []
+        for map_name in map_names:
+            map_patches = self._generate_patches_for_map(map_name, mapsh5i)
+            target_num_patches = len(mapsh5i[self.patch_size][map_name].get_patches(map_name))
+
+            selected_patches = []
+            while len(selected_patches) < target_num_patches:
+                patch = random.choice(map_patches)
+                if patch not in selected_patches:
+                    selected_patches.append(patch)
+                    map_patches.remove(patch)
+
+            all_patches.extend(selected_patches)
+        return all_patches
+
+    def _augment_data(self, data_patch, label_patch):
+        if random.random() < self.vertical_flip_rate:
+            data_patch = np.flipud(data_patch)
+            label_patch = np.flipud(label_patch)
+
+        if random.random() < self.horizontal_flip_rate:
+            data_patch = np.fliplr(data_patch)
+            label_patch = np.fliplr(label_patch)
+
+        if random.random() < self.rotation_rate:
+            angle = random.choice([0, 90, 180, 270])
+            data_patch = np.rot90(data_patch, k=angle // 90)
+            label_patch = np.rot90(label_patch, k=angle // 90)
+
+        if random.random() < self.hue_factor:
+            hue_delta = random.uniform(-self.hue_factor, self.hue_factor)
+            data_patch = TF.adjust_hue(torch.from_numpy(data_patch), hue_delta).numpy()
+
+        return data_patch, label_patch
+    
+    def _get_data_and_label(self, map_name, layer_name, row, column, h5image_obj):
+        map_patch = h5image_obj.get_patch(row, column, map_name)
+        map_patch = TF.resize(torch.from_numpy(map_patch), [self.patch_size, self.patch_size]).numpy()
+
+        legend_patch = h5image_obj.get_legend(map_name, layer_name)
+        legend_patch = TF.resize(torch.from_numpy(legend_patch), [self.patch_size, self.patch_size]).numpy()
+
+        label_patch = h5image_obj.get_patch(row, column, map_name, layer_name)
+        label_patch = resize(label_patch, (self.patch_size, self.patch_size), order=0, preserve_range=True, anti_aliasing=False) 
+
+        if self.norm_type == 'imagenet':
+            normalizer = ImageNetNormalizer()
+            map_patch = normalizer.normalize(map_patch)
+            legend_patch = normalizer.normalize(legend_patch)
+
+        elif self.norm_type == 'basic': 
+            normalizer = MinMaxNormalizer(0, 255, -1, 1)
+            map_patch = normalizer.normalize(map_patch)
+            legend_patch = normalizer.normalize(legend_patch)
+
+        else:  
+            normalizer = BasicNormalizer()
+            map_patch = normalizer.normalize(map_patch)
+            legend_patch = normalizer.normalize(legend_patch)
+
+        data_patch = np.concatenate([map_patch, legend_patch], axis=-1)
+
+        if self.augment:
+            data_patch, label_patch = self._augment_data(data_patch, label_patch)
+
+        return data_patch, label_patch
+
+    def __len__(self):
+        return len(self.patches)
+
+    def __getitem__(self, idx):
+        map_name, layer_name, row, column, size = self.patches[idx]
+        data, label = self._get_data_and_label(map_name, layer_name, row, column, self.mapsh5i[size])
+        data = torch.from_numpy(data).float()
+        label = torch.from_numpy(label).float()
+        return data, label
+
+    @staticmethod
+    def create_train_dataset(batch_size=32, patch_size=256, overlap=15, norm_type='imagenet', valid_patch_rate=0.75,
+                             augment=True, vertical_flip_rate=0.4, horizontal_flip_rate=0.4, rotation_rate=0.4, hue_factor=0.2, test_flag=False):
+        train_dir = '/projects/bbym/shared/data/commonPatchData/'
+        mapsh5i, map_names = PatchDataGenerator._load_maps_static(train_dir, patch_size, overlap)
+        return DataLoader(PatchDataGenerator(map_names, mapsh5i, batch_size, patch_size, overlap, norm_type, valid_patch_rate,
+                                             augment, vertical_flip_rate, horizontal_flip_rate, rotation_rate, hue_factor, test_flag), 
+                          batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+
+    @staticmethod
+    def create_validation_dataset(batch_size=32, patch_size=256, overlap=15, norm_type='imagenet', valid_patch_rate=0.75,
+                                  augment=True, vertical_flip_rate=0.4, horizontal_flip_rate=0.4, rotation_rate=0.4, hue_factor=0.2, test_flag=False):
+        validation_dir = '/projects/bbym/shared/data/commonPatchData/validation/'
+        mapsh5i, map_names = PatchDataGenerator._load_maps_static(validation_dir, patch_size, overlap)
+        return DataLoader(PatchDataGenerator(map_names, mapsh5i, batch_size, patch_size, overlap, norm_type, valid_patch_rate,
+                                             augment, vertical_flip_rate, horizontal_flip_rate, rotation_rate, hue_factor, test_flag), 
+                          batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+
+    @staticmethod
+    def _load_maps_static(h5_dir, patch_size, overlap):
+        mapsh5i = {}
+        exclude_files = [
+            "24_Black Crystal_2014_p43.hdf5", "26_Black Crystal_2018_6.hdf5",
+            "46_Coosa_2015_11 74.hdf5", "DMEA2328_OldLeydenMine_CO.hdf5",
+            "AZ_PrescottNF_basemap.hdf5", "CarlinVanadiumNI43-101Jun2020.hdf5",
+            "Genesis_fig7_1.hdf5", "Genesis_fig9_1.hdf5", "Genesis_fig9_2.hdf5",
+            "Genesis_fig9_3.hdf5", "Genesis_fig9_4.hdf5", "Genesis_fig9_5.hdf5",
+            "Titac_2011_fig7_6.hdf5", "Trend_2005_fig4_3.hdf5", "Trend_2005_fig6_4.hdf5",
+            "Trend_2005_fig7_2.hdf5", "Trend_2007_fig10_2.hdf5", "USGS_GF-7_2.hdf5",
+            "CA_SanJone"
+        ]
+
+        patch_sizes = [128, 256, 512, 1024]  # Supported patch sizes
+
+        for size in patch_sizes:
+            sub_dir = os.path.join(h5_dir, str(size), str(overlap))
+            size_mapsh5i = {}
+            files_name = [f for f in glob.glob(os.path.join(sub_dir, "*.hdf5")) if os.path.basename(f) not in exclude_files]
+            for file in files_name:
+                path = os.path.join(sub_dir, file)
+                mapname = os.path.basename(path).replace(".hdf5", "")
+                size_mapsh5i[mapname] = H5Image(path, "r")
+            mapsh5i[size] = size_mapsh5i  
+
+        map_names = list(mapsh5i[patch_size].keys())  # Default to input patch size for map_names
+        return mapsh5i, map_names
---
+++ training_scale.py
@@ -0,0 +1,157 @@
+import os
+import argparse
+
+import torch
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
+from torch.utils.data import DataLoader
+
+import wandb
+import mlflow
+
+from models.unet import UNet
+from models.unetTransformer import U_Transformer
+
+# from libs.data_module import PatchDataModule
+from libs.data_scale_loader import ScaleDataGenerator
+from libs.callbacks import ValidPatchRateCallback
+from models.lit_training import LitTraining
+
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:512'
+
+tracking_uri = "https://criticalmaas.software-dev.ncsa.illinois.edu/"
+os.environ['MLFLOW_TRACKING_USERNAME'] = 'ncsa'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = 'hydrohpc'
+mlflow.set_tracking_uri(uri=tracking_uri)
+mlflow.set_experiment("nathan")
+
+
+def initialize_data_loaders(args):
+    train_loader = ScaleDataGenerator.create_train_dataset(
+        batch_size=args.batch_size, 
+        patch_size=args.patch_size, 
+        overlap=args.overlap, 
+        norm_type=args.norm_type, 
+        hue_factor=args.hue_factor, 
+        augment=args.augment,
+        valid_patch_rate=args.valid_patch_rate
+    )
+
+    val_loader = ScaleDataGenerator.create_validation_dataset(
+        batch_size=args.batch_size, 
+        patch_size=args.patch_size, 
+        overlap=args.overlap, 
+        norm_type=args.norm_type, 
+        hue_factor=args.hue_factor, 
+        augment=args.augment,
+        valid_patch_rate=args.valid_patch_rate
+    )
+
+    # Check if the datasets are empty
+    if len(train_loader.dataset) == 0:
+        raise ValueError("Training dataset is empty. Please check the data directory or preprocessing steps.")
+    if len(val_loader.dataset) == 0:
+        raise ValueError("Validation dataset is empty. Please check the data directory or preprocessing steps.")
+
+    return train_loader, val_loader
+
+
+def main(args):
+
+    # Start an MLflow run
+    with mlflow.start_run():
+
+        # Config for wandb
+        config = {
+            'batch_size': args.batch_size,
+            'learning_rate': args.learning_rate,
+            'num_epochs': args.num_epochs,
+            'patch_size': args.patch_size,
+            'overlap': args.overlap,
+            'norm_type': args.norm_type,
+            'hue_factor': args.hue_factor,
+            'valid_patch_rate': args.valid_patch_rate,
+            'augment': args.augment,
+            'num_workers': args.num_workers,
+            'dynamic_valid_patch_rate': args.dynamic_valid_patch_rate
+        }
+
+        # Log parameters
+        mlflow.log_params(config)
+
+        # Initialize model
+        model = U_Transformer(in_channels=6, classes=1)
+
+        # Initialize Lightning model
+        lit_model = LitTraining(model, args.learning_rate)
+        
+        # Initialize Wandb logger
+        wandb_logger = WandbLogger( project=args.project_name,
+                                    sync_tensorboard=False,
+                                    name=args.name_id,
+                                    config=config)
+
+        # Model checkpoint callback
+        checkpoint_callback = ModelCheckpoint(
+            monitor='val/loss',
+            dirpath=os.path.join(args.checkpoint_dir, args.name_id),
+            filename='u-transformer-best',
+            save_top_k=1,
+            mode='min',
+        )
+        early_stop_callback = EarlyStopping(monitor="val/loss", min_delta=0.00, patience=50, verbose=True, mode="min")
+
+        # Initialize data loaders
+        train_loader, val_loader = initialize_data_loaders(args)
+
+        # Trainer
+        trainer = Trainer(
+                    max_epochs=args.num_epochs,
+                    accelerator="gpu",
+                    devices=2, 
+                    precision=32,
+                    logger=wandb_logger,
+                    callbacks=[checkpoint_callback, early_stop_callback],
+                    resume_from_checkpoint=args.checkpoint_file if args.checkpoint_file else None,
+                )
+
+        # Training
+        trainer.fit(lit_model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train U-Transformer model for segmentation.')
+
+    # Data-related arguments
+    data_group = parser.add_argument_group('Data')
+    data_group.add_argument('--train_data_dir', type=str, default='/projects/bbym/shared/data/commonPatchData/', help='Directory for training data')
+    data_group.add_argument('--val_data_dir', type=str, default='/projects/bbym/shared/data/commonPatchData/validation/', help='Directory for validation data')
+    data_group.add_argument('--patch_size', type=int, default=256, help='Patch size for data generator')
+    data_group.add_argument('--overlap', type=int, default=15, help='Overlap for patches')
+    data_group.add_argument('--norm_type', type=str, choices=['basic', 'imagenet', 'none'], default='basic', help='Normalization type')
+    data_group.add_argument('--hue_factor', type=float, default=0.2, help='Hue factor for data augmentation')
+    data_group.add_argument('--valid_patch_rate', type=float, default=0.75, help='Initial valid patch rate')
+    data_group.add_argument('--augment', default=True, type=lambda x: (str(x).lower() == 'true'), help='Whether to use data augmentation')
+    data_group.add_argument('--num_workers', type=int, default=12, help='Number of workers for data loader')
+
+    # Training-related arguments
+    training_group = parser.add_argument_group('Training')
+    training_group.add_argument('--batch_size', type=int, default=16, help='Batch size for training')
+    training_group.add_argument('--learning_rate', type=float, default=1e-3, help='Learning rate')
+    training_group.add_argument('--num_epochs', type=int, default=3, help='Number of epochs to train')
+    training_group.add_argument('--dynamic_valid_patch_rate', default=True, type=lambda x: (str(x).lower() == 'true'), help='Dynamically update valid_patch_rate each epoch')
+
+    # Checkpoint and logging arguments
+    log_group = parser.add_argument_group('Logging')
+    log_group.add_argument('--checkpoint_dir', type=str, default='checkpoints', help='Directory to save checkpoints')
+    log_group.add_argument('--checkpoint_file', type=str, default=None, help='Path to the checkpoint file to resume training')
+    log_group.add_argument('--project_name', type=str, default='U_Transformer_Segmentation_scales', help='WandB project name')
+    log_group.add_argument('--name_id', type=str, default='experiment', help='WandB run name')
+
+    args = parser.parse_args()
+
+    main(args)
+
+
+# python training_scale.py --name_id "scale_experiment" --checkpoint_file "/u/nathanj/DARPA_pytorch/checkpoints/Utransformer_256_32_w_val_constant_rate/u-transformer-val/loss=0.29.ckpt"
